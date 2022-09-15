549b88d1230c6f2d17c8310ad670e3c042a266c6
ddo0ii <soyeong5857@gmail.com>
2022-09-15 17:26:36 +0900

    6.1 useEffect - useEffect사용

--- react-for-begginners/src/App.js
+++ react-for-begginners/src/App.js
@@ -1,9 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((pre) => pre + 1);
-  console.log("call an api");
+  console.log("I run all the time");
+  const iRunOnlyOnce = () => {
+    console.log("I run only once.");
+  };
+  useEffect(iRunOnlyOnce, []);
   return (
     <div>
       <h1>{counter}</h1>
