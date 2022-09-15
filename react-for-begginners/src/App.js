60d8e83ad69b82bf5f0f2cef73fa103473cfbd0e
ddo0ii <soyeong5857@gmail.com>
2022-09-15 17:29:44 +0900

    6.1 useEffect

--- react-for-begginners/src/App.js
+++ react-for-begginners/src/App.js
@@ -4,10 +4,9 @@ function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((pre) => pre + 1);
   console.log("I run all the time");
-  const iRunOnlyOnce = () => {
-    console.log("I run only once.");
-  };
-  useEffect(iRunOnlyOnce, []);
+  useEffect(() => {
+    console.log("Call the API...");
+  }, []);
   return (
     <div>
       <h1>{counter}</h1>
