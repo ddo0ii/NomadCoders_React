ddccd18bb239bda297020b18620ddfb2af0b8a07
ddo0ii <soyeong5857@gmail.com>
2022-09-15 15:36:26 +0900

    5.1 Tour of CRA - 버튼 생성, prop-types설치

--- react-for-begginners/src/App.js
+++ react-for-begginners/src/App.js
@@ -1,7 +1,10 @@
+import Button from "./Button";
+
 function App() {
   return (
     <div>
       <h1>Welcome back!</h1>
+      <Button text={"Continue"} />
     </div>
   );
 }
---
+++ react-for-begginners/src/Button.js
@@ -0,0 +1,11 @@
+import PropTypes from "prop-types";
+
+function Button({ text }) {
+  return <button>{text}</button>;
+}
+
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+};
+
+export default Button;
