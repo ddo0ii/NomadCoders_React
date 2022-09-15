04e74966ef044ce4ba97e246131652432538216f
ddo0ii <soyeong5857@gmail.com>
2022-09-15 15:52:30 +0900

    버튼 모듈로 css입히기

--- react-for-begginners/src/Button.js
+++ react-for-begginners/src/Button.js
@@ -1,7 +1,8 @@
 import PropTypes from "prop-types";
+import styles from "./Button.module.css";
 
 function Button({ text }) {
-  return <button>{text}</button>;
+  return <button className={styles.btn}>{text}</button>;
 }
 
 Button.propTypes = {
