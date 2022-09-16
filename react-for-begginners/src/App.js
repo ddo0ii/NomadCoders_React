a6dc8e9282a11aaf104999683c03fbe0b568c68a
ddo0ii <soyeong5857@gmail.com>
2022-09-16 15:28:21 +0900

    6.2 Deps - useEffect 조건을 줘서 keyword입력시, 5보다클때 실행

--- react-for-begginners/src/App.js
+++ react-for-begginners/src/App.js
@@ -2,13 +2,25 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((pre) => pre + 1);
-  console.log("I run all the time");
+  const onChange = (event) => setKeyword(event.target.value);
   useEffect(() => {
     console.log("Call the API...");
   }, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("SEARCH FOR", keyword);
+    }
+  }, [keyword]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>Click me</button>
     </div>
