7dc7c7401739ad02e1a9fc547f6280f7a8859fca
ddo0ii <soyeong5857@gmail.com>
2022-09-16 17:22:02 +0900

    6.4 Cleanup

--- react-for-begginners/src/App.js
+++ react-for-begginners/src/App.js
@@ -2,28 +2,10 @@ import { func } from "prop-types";
 import { useEffect, useState } from "react";
 
 function Hello() {
-  function byeFn() {
-    console.log("bye :(");
-  }
-  function hiFn() {
-    console.log("created :)");
-    return byeFn;
-  }
-
-  // 아래의 방법을 많이 사용한다
-  // 방법1 (이것이 더 짧기 때문에 이것을 더 많이 사용)
   useEffect(() => {
     console.log("hi :)");
     return () => console.log("bye :(");
   }, []);
-  // 방법2
-  useEffect(function () {
-    console.log("hi :)");
-    return function () {
-      console.log("bye :(");
-    };
-  }, []);
-
   return <h1>Hello</h1>;
 }
 
