72fcd951d946978c827a02cbbdf0c9d3d21167ef
ddo0ii <soyeong5857@gmail.com>
2022-09-16 17:23:16 +0900

    create,destroy using useEffect

---
+++ react-for-begginners/src/practiceFiles/create_destroy_fromUseEffect.js
@@ -0,0 +1,23 @@
+import { func } from "prop-types";
+import { useEffect, useState } from "react";
+
+function Hello() {
+  useEffect(() => {
+    console.log("hi :)");
+    return () => console.log("bye :(");
+  }, []);
+  return <h1>Hello</h1>;
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
+  return (
+    <div>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+    </div>
+  );
+}
+
+export default App;
