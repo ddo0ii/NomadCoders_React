2cd138bd52bd25f60e692e13b59e5e16167ca803
ddo0ii <soyeong5857@gmail.com>
2022-09-17 14:01:05 +0900

    7.0 To Do List part One - input, form, onSubmit사용

--- react-for-begginners/src/App.js
+++ react-for-begginners/src/App.js
@@ -1,20 +1,28 @@
-import { useEffect, useState } from "react";
-
-function Hello() {
-  useEffect(() => {
-    console.log("hi :)");
-    return () => console.log("bye :(");
-  }, []);
-  return <h1>Hello</h1>;
-}
+import { useState } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    // input이 빌때 작동하지 않기
+    if (toDo === "") {
+      return;
+    }
+    // 비우기
+    setToDo("");
+  };
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
