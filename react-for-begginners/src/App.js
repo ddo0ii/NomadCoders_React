6ece150a0d2ab5dbdf1d6a56fe50c13279bb9838
ddo0ii <soyeong5857@gmail.com>
2022-09-19 18:59:18 +0900

    7.5 React Router - router사용하기

--- react-for-begginners/src/App.js
+++ react-for-begginners/src/App.js
@@ -1,5 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
+
 function App() {
-  return null;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/movie">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
