20a3879ecfd2d76161263a555e92208e3ba2b123
ddo0ii <soyeong5857@gmail.com>
2022-09-19 17:07:06 +0900

    async, awiat로 바꾸기

--- react-for-begginners/src/App.js
+++ react-for-begginners/src/App.js
@@ -3,15 +3,17 @@ import { useEffect, useState } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year `
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
   useEffect(() => {
-    fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year `
-    )
-      .then((response) => response.json())
-      .then((json) => {
-        setMovies(json.data.movies);
-        setLoading(false);
-      });
+    getMovies();
   }, []);
   console.log(movies);
   return <div>{loading ? <h1>Loading...</h1> : null}</div>;
