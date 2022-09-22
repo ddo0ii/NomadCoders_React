5744c6f4840226fef549dbfa68ea520589920aa2
ddo0ii <soyeong5857@gmail.com>
2022-09-22 15:18:24 +0900

    11.0 Class Components and State - js로 버튼만들기

--- movie_app_2019/src/App.js
+++ movie_app_2019/src/App.js
@@ -2,8 +2,23 @@ import React from "react";
 import PropTypes from "prop-types";
 
 class App extends React.Component {
+  state = {
+    count: 0,
+  };
+  add = () => {
+    console.log("add");
+  };
+  minus = () => {
+    console.log("minus");
+  };
   render() {
-    return <h1>I'm a class component</h1>;
+    return (
+      <div>
+        <h2>The number is {this.state.count}</h2>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
   }
 }
 
