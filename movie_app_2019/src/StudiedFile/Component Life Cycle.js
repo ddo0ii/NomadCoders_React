3f59a9616b31e0334a8561aacbb4276d00d4b22a
ddo0ii <soyeong5857@gmail.com>
2022-09-22 16:03:09 +0900

    Studied file upload

---
+++ movie_app_2019/src/StudiedFile/Component Life Cycle.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("hello");
+  }
+  state = {
+    count: 0,
+  };
+  add = () => {
+    this.setState((current) => ({ count: current.count + 1 }));
+  };
+  minus = () => {
+    this.setState((current) => ({ count: current.count - 1 }));
+  };
+  componentDidMount() {
+    console.log("component rendered");
+  }
+  componentDidUpdate() {
+    console.log("I just updated");
+  }
+  componentWillUnmount() {
+    console.log("Goodbye, cruel world");
+  }
+  render() {
+    console.log("I'm rendering");
+    return (
+      <div>
+        <h2>The number is {this.state.count}</h2>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
+}
+
+export default App;
