1deb6a99b1659f4f904689931e7b3577b5edde92
ddo0ii <soyeong5857@gmail.com>
2022-09-28 16:03:49 +0900

    1.3 useTabs

--- reacthook/src/useTabs.js
+++ reacthook/src/useTabs.js
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 const content = [
   {
     tab: "Section 1",
@@ -11,7 +9,7 @@ const content = [
   },
 ];
 
-const useTabs = (initialTab, allTabs) => {
+export const useTabs = (initialTab, allTabs) => {
   const [currentIndex, setCurrentIndex] = useState(initialTab);
 
   if (!allTabs || !Array.isArray(allTabs)) {
@@ -23,18 +21,3 @@ const useTabs = (initialTab, allTabs) => {
     changeItem: setCurrentIndex,
   };
 };
-
-const App = () => {
-  const { currentItem, changeItem } = useTabs(0, content);
-
-  return (
-    <div>
-      {content.map((section, index) => (
-        <button onClick={() => changeItem(index)}>{section.tab}</button>
-      ))}
-      <div>{currentItem.content}</div>
-    </div>
-  );
-};
-
-export default App;
