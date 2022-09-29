d4e5026d32d7e33beee0ece941516a3671190f82
ddo0ii <soyeong5857@gmail.com>
2022-09-29 15:24:18 +0900

    2.3 useConfirm & usePreventLeave - useConfirm

--- reacthook/src/useConfirm_full.js
+++ reacthook/src/useConfirm_full.js
@@ -1,10 +1,13 @@
-export const useConfirm = (message = "", onConfirm, onCancel) => {
-  if (onConfirm || typeof onConfirm !== "function") {
+import React, { useState, useEffect, useRef } from "react";
+
+const useConfirm = (message = "", onConfirm, onCancel) => {
+  if (!onConfirm || typeof onConfirm !== "function") {
     return;
   }
-  if (onCancel && typeof onCancel !== "function") {
+  if (!onCancel || typeof onCancel !== "function") {
     return;
   }
+
   const confirmAction = () => {
     if (confirm(message)) {
       onConfirm();
@@ -12,5 +15,18 @@ export const useConfirm = (message = "", onConfirm, onCancel) => {
       onCancel();
     }
   };
+
   return confirmAction;
 };
+
+export default function App() {
+  const deleteworld = () => console.log("Deleting the world!...");
+  const abort = () => console.log("Aborted");
+  const confirmDelete = useConfirm("Are you sure?", deleteworld, abort);
+
+  return (
+    <div className="App">
+      <button onClick={confirmDelete}>Delete the world!</button>
+    </div>
+  );
+}
