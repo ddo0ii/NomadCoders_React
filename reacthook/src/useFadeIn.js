8d497c4efaf6de5b1b40b1636dcd87e958fcdff8
ddo0ii <soyeong5857@gmail.com>
2022-09-29 18:41:42 +0900

    useFadeIn

--- reacthook/src/useFadeIn.js
+++ reacthook/src/useFadeIn.js
@@ -1,11 +1,12 @@
-// hook이랑 animation을 섞는 방법!
-export const useFadeIn = (duration = 1, delay = 0) => {
+import React, { useEffect, useRef } from "react";
+
+const useFadeIn = (duration = 1, delay = 0) => {
   if (typeof duration !== "number" || typeof delay !== "number") {
     return;
   }
-
   const element = useRef();
   useEffect(() => {
+    // element 안으로 들어가기 위해서 useEffect 사용
     if (element.current) {
       const { current } = element;
       current.style.transition = `opacity ${duration}s ease-in-out ${delay}s`;
@@ -14,3 +15,16 @@ export const useFadeIn = (duration = 1, delay = 0) => {
   }, []);
   return { ref: element, style: { opacity: 0 } };
 };
+
+const App = () => {
+  const fadeInH1 = useFadeIn(1);
+  const fadeInP = useFadeIn(1, 0.5); // delay 옵션 추가
+  return (
+    <div className="App">
+      <h1 {...fadeInH1}> Hello </h1>
+      <p {...fadeInP}>Welcome</p>
+    </div>
+  );
+};
+
+export default App;
