cb2021b27b1092d9af654cf9acdd496c539bbc05
ddo0ii <soyeong5857@gmail.com>
2022-09-29 18:19:48 +0900

    useClick

--- reacthook/src/useClick.js
+++ reacthook/src/useClick.js
@@ -1,27 +1,21 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useRef } from "react";
 
 const useClick = (onClick) => {
-  // onClick function이 비면 아무것도 실행하지 않음!
-  if (typeof onClick !== "function") {
-    return;
-  }
-
   const element = useRef();
-
   useEffect(() => {
-    // mount, update시 실행
-    // dependency 없으므로 update는 고려 안하겠지!
-    if (element.current) {
-      element.current.addEventListener("click", onClick);
+    if (typeof onClick === "function") {
+      // componentDidMount, componentDidUpdate 일 때 실행하는 부분
+      if (element.current) {
+        element.current.addEventListener("click", onClick);
+      }
     }
-    // function을 return 받았다는 것은, componentWillUmnount로부터 받은 것
     return () => {
+      // componentWillUnmount 일 때 실행하는 부분
       if (element.current) {
         element.current.removeEventListener("click", onClick);
       }
     };
   }, []);
-
   return element;
 };
 
