a2c5cc82313858d68676120723d00d050518a3f4
ddo0ii <soyeong5857@gmail.com>
2022-09-29 15:58:34 +0900

    2.5 useFadeIn & useNetwork - useFadeIn

--- reacthook/src/useFadeIn.js
+++ reacthook/src/useFadeIn.js
@@ -1,7 +1,5 @@
-import React, { useState, useEffect, useRef } from "react";
-
 // hook이랑 animation을 섞는 방법!
-const useFadeIn = (duration = 1, delay = 0) => {
+export const useFadeIn = (duration = 1, delay = 0) => {
   if (typeof duration !== "number" || typeof delay !== "number") {
     return;
   }
@@ -16,15 +14,3 @@ const useFadeIn = (duration = 1, delay = 0) => {
   }, []);
   return { ref: element, style: { opacity: 0 } };
 };
-
-export default function App() {
-  const fadeInH1 = useFadeIn(1, 2);
-  const fadeInP = useFadeIn(5, 10);
-
-  return (
-    <div className="App">
-      <h1 {...fadeInH1}>Hello</h1>
-      <p {...fadeInP}>lorem ipsum lalalalal</p>
-    </div>
-  );
-}
