69c2f0da51b7e79f2bf60f0de29f035d3f518bcb
ddo0ii <soyeong5857@gmail.com>
2022-09-29 18:20:55 +0900

    useTabs

--- reacthook/src/useTabs.js
+++ reacthook/src/useTabs.js
@@ -1,23 +1,37 @@
+import React, { useState } from "react";
+
 const content = [
   {
     tab: "Section 1",
-    content: "I'm content of the Section 1",
+    content: "This is the content of the Section 1",
   },
   {
     tab: "Section 2",
-    content: "I'm content of the Section 2",
+    content: "This is the content of the Section 2",
   },
 ];
 
-export const useTabs = (initialTab, allTabs) => {
-  const [currentIndex, setCurrentIndex] = useState(initialTab);
-
+const useTabs = (initialTab, allTabs) => {
   if (!allTabs || !Array.isArray(allTabs)) {
-    return;
+    return; // useTabs를 사용할 때 두번째 매개변수를 주지 않거나, 두번째 매개변수가 매열이 아니라면 return(에러 페이지 뜨지 않게)
   }
-
+  const [currentIndex, setCurrentIndex] = useState(initialTab);
   return {
     currentItem: allTabs[currentIndex],
     changeItem: setCurrentIndex,
   };
 };
+
+const App = () => {
+  const { currentItem, changeItem } = useTabs(0, content);
+  return (
+    <div className="App">
+      {content.map((section, index) => (
+        <button onClick={() => changeItem(index)}>{section.tab}</button>
+      ))}
+      <div>{currentItem.content}</div>
+    </div>
+  );
+};
+
+export default App;
