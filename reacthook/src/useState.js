18c9d137ffd677edbe782c90b7a146179e5be2d3
ddo0ii <soyeong5857@gmail.com>
2022-09-29 18:20:42 +0900

    useState

--- reacthook/src/useState.js
+++ reacthook/src/useState.js
@@ -2,15 +2,14 @@ import { useState } from "react";
 
 const App = () => {
   const [item, setItem] = useState(1);
-  const incrementItem = () => setItem(item + 1);
-  const decrementItem = () => setItem(item - 1);
-
+  const increament = () => setItem(item + 1);
+  const decreament = () => setItem(item - 1);
   return (
     <div className="App">
       <h1>Hello {item}</h1>
       <h2>Start editing to see some magic happen!</h2>
-      <button onClick={incrementItem}>Increment</button>
-      <button onClick={decrementItem}>Decrement</button>
+      <button onClick={increament}>increament</button>
+      <button onClick={decreament}>decreament</button>
     </div>
   );
 };
--- reacthook/src/useState_ugly.js
+++ reacthook/src/useState_ugly.js
@@ -1,37 +1,33 @@
 import React from "react";
-import ReactDOM from "react-dom";
+
 class AppUgly extends React.Component {
   state = { item: 1 };
+
   render() {
     const { item } = this.state;
     return (
       <div className="App">
         <h1>Hello {item}</h1>
         <h2>Start editing to see some magic happen!</h2>
-        <button onClick={this.incrementItem}>Increment</button>
-        <button onClick={this.decrementItem}>Decrement</button>
+        <button onClick={this.increament}>increament</button>
+        <button onClick={this.decreament}>decreament</button>
       </div>
     );
   }
+  increament = () => {
+    this.setState((state) => {
+      return {
+        item: state.item + 1,
+      };
+    });
+  };
+  decreament = () => {
+    this.setState((state) => {
+      return {
+        item: state.item - 1,
+      };
+    });
+  };
 }
 
-incrementItem = () => {
-  this.setState((state) => {
-    return {
-      item: state.item + 1,
-    };
-  });
-};
-
-decrementItem = () => {
-  this.setState((state) => {
-    return {
-      item: state.item + 1,
-    };
-  });
-};
-
-export default App;
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<AppUgly />, rootElement);
+export default AppUgly;
