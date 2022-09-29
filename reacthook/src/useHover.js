382ac2b5b66716ecdaf814b97dcd778f81ca13a8
ddo0ii <soyeong5857@gmail.com>
2022-09-29 18:24:00 +0900

    useHover

---
+++ reacthook/src/useHover.js
@@ -0,0 +1,20 @@
+const useHover = (onHover) => {
+  if (typeof onHover !== "function") {
+    return;
+  }
+
+  const element = useRef();
+
+  useEffect(() => {
+    if (element.current) {
+      element.current.addEventListener("mouseenter", onHover);
+    }
+    return () => {
+      if (element.current) {
+        element.current.removeEventListener("mouseenter", onHover);
+      }
+    };
+  }, []);
+
+  return element;
+};
