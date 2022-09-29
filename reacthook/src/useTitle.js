bc2eaa5ae4c5e3c7577473dda04abe0d9aef9807
ddo0ii <soyeong5857@gmail.com>
2022-09-29 18:21:11 +0900

    useTitle

--- reacthook/src/useTitle.js
+++ reacthook/src/useTitle.js
@@ -1,9 +1,26 @@
-export const useTitle = (initialTitle) => {
-  const [title, setTitle] = useState(initialTitle);
+import { useEffect, useState } from "react";
+
+const useTitle = (iniitialTitle) => {
+  const [title, setTitle] = useState(iniitialTitle);
   const updateTitle = () => {
     const htmlTitle = document.querySelector("title");
     htmlTitle.innerText = title;
   };
-  useEffect(updateTitle, [title]); // mount 될 때 updateTitle 실행
+  useEffect(updateTitle, [title]);
   return setTitle;
 };
+
+const App = () => {
+  const titleUpdater = useTitle("My React App...");
+  setTimeout(() => {
+    // 3초후 titleUpdater 실행
+    titleUpdater("home");
+  }, 3000);
+  return (
+    <div className="App">
+      <div>Hi</div>
+    </div>
+  );
+};
+
+export default App;
