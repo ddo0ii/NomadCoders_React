ad52ca6473580bd1c5a2479aeccb425580481275
ddo0ii <soyeong5857@gmail.com>
2022-09-30 14:53:41 +0900

    useNotification

---
+++ reacthook/src/useNotification .js
@@ -0,0 +1,30 @@
+const useNotification = (title, options) => {
+  if (!("Notification" in window)) {
+    return;
+  }
+  const fireNotif = () => {
+    if (Notification.permission !== "granted") {
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+          new Notification(title, options);
+        } else {
+          return;
+        }
+      });
+    } else {
+      new Notification(title, options);
+    }
+  };
+  return fireNotif;
+};
+
+const App = () => {
+  const triggerNotif = useNotification("Can I steal your kimchi?");
+  return (
+    <div style={{ heigh: "1000vh" }}>
+      <button onClick={triggerNotif}>Hello</button>
+    </div>
+  );
+};
+
+export default App;
